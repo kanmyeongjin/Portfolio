9becb1ffaff1f29958a3afe8d933f4c402f948b4
mjkan <rksaudwls1@naver.com>
2024-01-02 14:54:31 +0900

    init - 부트스트랩 적용 및 게시판 연동

---
+++ src/main/java/com/example/board/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.example.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/projects")
+    public String projects(){
+        System.out.println("Projects");
+        return "projects";
+    }
+}
